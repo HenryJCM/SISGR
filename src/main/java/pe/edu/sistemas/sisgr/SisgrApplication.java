f54a0d0e1ff874640e458e7f74f83c72ef62d5fd
HenryJCM <henjacolonia17@gmail.com>
2018-04-13 17:54:25 -0500

    proyecto iniciado

---
+++ src/main/java/pe/edu/sistemas/sisgr/SisgrApplication.java
@@ -0,0 +1,12 @@
+package pe.edu.sistemas.sisgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisgrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisgrApplication.class, args);
+	}
+}
